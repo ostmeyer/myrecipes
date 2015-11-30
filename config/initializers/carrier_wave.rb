94766b581136f2db64eab89321314708d4813834
ChuckO <work@ostmeyer.net>
2015-11-29 23:40:06 -0800

    fixed typo in carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,8 @@ if Rails.env.production?
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
 
-    config.fog.directory = ENV['S3_BUCKET']
-    
+    config.fog_directory = ENV['S3_BUCKET']
+
   end
 
 end
